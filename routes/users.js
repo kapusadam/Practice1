25f1dd7933aaa06931897eb3878c694814fef0a8
Adam Kruppa <Adam Kruppa>
2016-04-16 22:22:12 +0200

    Mongo added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost/cm';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  MongoClient.connect(url, function(err, db) {
+    if(err) {
+      res.send('ERROR');
+      return;
+    }
+    var cursor = db.collection('books').find().toArray(function(err, items) {
+      if(err) {
+        res.send('ERROR');
+        return;
+      }
+      res.json(items);
+    });
+  });
 });
 
 module.exports = router;
