11a42bbe333ec931b141a414d3d79ffcf2a3c4b9
Adam Kruppa <Adam Kruppa>
2016-04-16 22:27:53 +0200

    Test

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://localhost/cm';
+
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
   MongoClient.connect(url, function(err, db) {
     if(err) {
