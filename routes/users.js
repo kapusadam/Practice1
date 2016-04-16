24e5605d2c387ffff0af3956f780a9b683bf1d41
Adam Kruppa <Adam Kruppa>
2016-04-16 22:24:28 +0200

    Authors

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost/cm';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,7 +10,7 @@ router.get('/', function(req, res, next) {
       res.send('ERROR');
       return;
     }
-    var cursor = db.collection('books').find().toArray(function(err, items) {
+    db.collection('authors').find().toArray(function(err, items) {
       if(err) {
         res.send('ERROR');
         return;
