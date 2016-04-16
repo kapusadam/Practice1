f31297b61eb3b80e368c8f846f26d8e064a2f57e
Adam Kruppa <Adam Kruppa>
2016-04-16 22:30:42 +0200

    Test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+router.get('/env', function(req, res, next) {
+  res.send('env');
+});
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a mocked resource');
 });
 
 module.exports = router;
